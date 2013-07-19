b17efa61e5382983490060a83bd8deb803e83149
joris-veens <joris@scriptmotion.nl>
2013-07-19 13:03:34 +0200

    wc_views geplande functies
    get_views()
    add_views()
    delete_views()
    check_ip()
    add_ip()
    
    als een bezoeker de eerste keer op de pagina komt wordt het ip address
    opgeslagen, en worden de available_views van die trainer met 1
    verminderd. De total_views van de trainer stijgen met  1.

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -3,14 +3,34 @@
 class Test extends CI_Controller
 {
       function __construct()
-        {
+      {
             parent::__construct();
             $this -> load -> model('wc_views', 'views');
-        }
+            $this -> views -> check_ip( 1, $this -> input -> ip_address() );
+      }
         
+      function index()
+      {
+          echo 'nothing to see here';
+          
+      }
+      
       function views()
       {
-          $this -> views -> get_remaining_views(1);
+          echo $this -> log -> show_messages();
+          echo $this -> views -> get_views(1) -> available_views;
       }
+      
+      function add_views()
+      {
+          $data = array(
+            'trainer_id' => 1,
+            'amount'     => 50
+          );
+          $this -> views -> add_views($data);
+          echo $this -> log -> show_messages();
+          echo $this -> views -> get_views(1) -> available_views;
+      }
+      
 }
 ?>
--- application/models/wc_views.php
+++ application/models/wc_views.php
@@ -7,15 +7,15 @@
             parent::__construct();
         }
         
-        function get_remaining_views( $trainer_id = false )
+        function get_views( $trainer_id = false )
         {
             if( !$trainer_id )
             {
                 $this -> log -> add_message('geen trainer geselecteerd');
                 return false;
             }
-                           $this -> db -> select('available_views');
-                           $this -> db -> where('trainer_id', $trainer_id);
+                           $this -> db -> select('available_views, total_views');
+                           $this -> db -> where('id', $trainer_id);
             if( !$result = $this -> db -> get('trainers') )
             {
                  $this -> log -> add_message('kon geen verbinding maken met de database');
@@ -28,23 +28,125 @@
                 return false;
             }
             
-            return $result -> row() -> available_views;
+            return $result -> row();
             
         }
         
         function add_views( $data = false )
         {
             
+            if( !$data['trainer_id'] || !$data['amount'])
+            {
+                echo 'false';
+                $this -> log -> add_message('niet alle gegevens zijn opgegeven.');
+                return false;
+            }
+            
+            if( !$old_views = $this -> get_views($data['trainer_id']) -> available_views ) return false;
+            
+            $new_views = (int)$old_views + $data['amount'];
+            
+            $update_data = array(
+                'available_views' => $new_views
+            );
+            
+                 $this -> db -> where('id', $data['trainer_id']);
+            if( !$this -> db -> update('trainers', $update_data) )
+            {
+                $this -> log -> add_message('kon geen verbinding met database maken.');
+                return false;
+            }
+            
+            return true;
         }
         
-        function delete_views( $trainer_id )
+        function delete_views( $trainer_id = false )
         {
+            if( !$trainer_id )
+            {
+                $this -> log -> add_message('geen trainer geselecteerd');
+                return false;
+            }
             
+           $views = $this -> get_views( $trainer_id );
+           if( !$views ) return false;
+           
+           (int)$views -> available_views--;
+           (int)$views -> total_views++;
+           
+           $data = array(
+             'available_views' => $views -> available_views,
+              'total_views'    => $views -> total_views
+           );
+           
+                $this -> db -> where('id', $trainer_id);
+           if( !$this -> db -> update('trainers', $data) )
+           {
+               $this -> log -> add_message('kon geen verbinding maken met database');
+               return false;
+           }
+           
+           return true;
         }
         
-        function check_ip( $trainer_id )
+        function check_ip( $trainer_id = false, $ip_address = false )
         {
+             if( !$trainer_id || !$ip_address )
+            {
+                $this -> log -> add_message('geen trainer geselecteerd');
+                return false;
+            }
             
+            $this -> db -> where('trainer_id', $trainer_id);
+            if( !$result=$this -> db -> get('views_ip') )
+            {
+                $this -> log -> add_message('kon geen verbinding met de database maken');
+                return false;
+            }
+            
+            if( !$result -> num_rows() )
+            {
+                 if( !$this -> delete_views( $trainer_id ) ) return false;
+                 if( !$this -> add_ip( $trainer_id, $ip_address ) ) return false;
+                 return true;
+            }
+            
+            $ip_match = false;
+            
+            foreach($result -> result() as $row)
+            {
+                if( $row -> ip_address == $ip_address ) $ip_match = true;
+            } 
+                
+            if( !$ip_match )
+            {
+                if( !$this -> delete_views( $trainer_id ) ) return false;
+                if( !$this -> add_ip( $trainer_id, $ip_address ) ) return false;
+            }
+            
+            return true;
+        }
+        
+        function add_ip( $trainer_id= false, $ip_address = false )
+        {
+            if( !$trainer_id || !$ip_address )
+            {
+                $this -> log -> add_message('geen trainer geselecteerd');
+                return false;
+            }
+            
+             $data = array(
+                 'trainer_id' => $trainer_id,
+                 'ip_address' => $ip_address
+             );
+             
+             if( !$this -> db -> insert('views_ip', $data) )
+             {
+                 $this -> log -> add_message('kan geen verbinding maken met database.');
+                 return false;
+             }
+             
+             return true;
         }
         
     }
