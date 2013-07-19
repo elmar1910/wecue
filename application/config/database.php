e6384aae4b8b60d153c24f7b2621915ad6e41af2
Elmar Puts <elmar@scriptmotion.nl>
2013-07-19 13:23:36 +0200

    database.php weg

--- application/config/database.php
+++ application/config/database.php
@@ -72,7 +72,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'sdjdslg';
 $db['default']['database'] = 'wecue';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
