9e11b3024265ea7657db851788da49df8483b692
joris-veens <joris@scriptmotion.nl>
2013-07-29 15:31:54 +0200

    trainer_profile

---
+++ application/views/trainer_profile.php
@@ -0,0 +1,15 @@
+ <div class="simplebox grid740">
+                        
+    <div class="titleh"><h3><?php echo $trainer -> name ?></h3></div>
+                            
+    <div class="body">
+        naam:           <?php echo $trainer -> name ?>                <br />
+        adres:          <?php echo $trainer -> address ?>             <br />
+        woonplaats:     <?php echo $trainer -> city ?>                <br />
+        telefoon:       <?php echo $trainer -> phone ?>               <br />
+        website:        <?php echo $trainer -> website ?>             <br />
+        beschrijving:   <?php echo $trainer -> short_description ?>   <br />
+        uurtarief:      <?php echo $trainer -> salary ?>              <br />
+    </div>                 
+                            
+</div>
